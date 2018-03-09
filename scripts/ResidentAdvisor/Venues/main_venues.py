d10fef24424ce432b40b1b05ab13902557db745c
nequalstim <tim.flecki@gmail.com>
2018-03-09 09:31:53 +0100

    Enahce documentation

--- scripts/ResidentAdvisor/Venues/main_venues.py
+++ scripts/ResidentAdvisor/Venues/main_venues.py
@@ -1,3 +1,11 @@
+"""
+Start point of the scraper, where user gives the names of the countries he would like 
+to have scraped as a command line argument. 
+
+The corresponding IDs of each country are stored in a dictionary and subsequently scraped for 
+their venue links. These venue_links are handed over to the next method then -> scrape_links_venue
+"""
+
 import sys 
 import os 
 sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__))+ '/..'))
@@ -23,16 +31,14 @@ for country, ids in country_ids.items():
     RA_Id = ids['ra_id']
     end = ids['end']
 
-    links = []
+    venue_links = []
 
-    # get all the venue links from all of COUNTRY and save them to links
+    # get all the venue links from all of COUNTRY and save them to links array
     for x, y in zip(RA_Id, end):
-        #extract all the links of venues for a given country
-        get_all_links(x, y, links, "venue")
+        """ extract all the links of venues for a given country
+            some countries just have on site on RA (Cuba)
+            some are divided into multiple (Germany -> Bavaria, Berlin etc.) """
+        get_all_links(x, y, venue_links, "venue")
 
     #scrape the collected links 
-    scrape_links_venue(links, country)
-
-
-
-
+    scrape_links_venue(venue_links, country)
