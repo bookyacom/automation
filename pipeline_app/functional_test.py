6eb7d0e855844b3bcbfee43c79a968e2d7a76174
alvaro-alonso <a.alonsodomenech@yahoo.com>
2018-04-12 19:17:08 +0200

    app insert data

--- pipeline_app/functional_test.py
+++ pipeline_app/functional_test.py
@@ -6,6 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
@@ -17,10 +18,10 @@ class NewVisitorTest(unittest.TestCase):
         # Amased by the first impression, he looks to the tab title to make sure it's real
         self.assertIn('pipeline - 4 real!', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Latest Entries', header_text)
+        self.assertIn('Scraped', header_text)
 
         # Eelke sees he can edit some data himself
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('edit_data')
         self.assertEqual(
             inputbox.get_attribute('placeholder'), 'Modify data')
 
@@ -30,11 +31,14 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('NL')
 
         # When he press enter, the site updates, and the data is changed
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
+        table = self.browser.find_element_by_id("artist_table")
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(row.text == 'NL')
+        self.assertIn('NL', [row.text for row in rows],
+                        'New info did not appear in the table -- its text was:\n%s' % (
+                            table.text,
+                        ))
 
         # Eelke sees more mistake in the data, but he is going for lunch first
         self.fail('Needs implementation')
--- pipeline_app/pipelines/tests.py
+++ pipeline_app/pipelines/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from pipelines.views import home_page
 
@@ -11,7 +12,11 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
